ce400e3285d9c64ec9ece6b3f0e113dd5c4dc993
JungDongSeob <ehdtjqzz@naver.com>
2023-11-20 16:20:22 +0900

    [Feat] ex04 기능 구현 자바연습입니다.

--- Ex04.java
+++ Ex04.java
@@ -4,6 +4,7 @@ public class RSC {
     public static void main(String[] args) {
         Player player01;
         Player player02;
+        int a=1;
 
         player01 = new Player();
         player02 = new Player();
@@ -12,7 +13,7 @@ public class RSC {
         player02.Player = "player02";
 
         // 1은 가위, 2는 바위, 3은 보
-        while (true) {
+        while (a==1) {
             player01.RSC = (int) (Math.random() * 3) + 1;
             player02.RSC = (int) (Math.random() * 3) + 1;
 
@@ -22,28 +23,28 @@ public class RSC {
             if (player01.RSC == 1) {
                 if (player02.RSC == 2) {
                     System.out.println("player02 win!");
-                    break;
+                    a=0;
                 } else if (player02.RSC == 3) {
                     System.out.println("player01 win!");
-                    break;
+                    a=0;
                 } else
                     System.out.println("무승부");
             } else if (player01.RSC == 2) {
                 if (player02.RSC == 3) {
                     System.out.println("player02 win!");
-                    break;
+                    a=0;
                 } else if (player02.RSC == 1) {
                     System.out.println("player01 win!");
-                    break;
+                    a=0;
                 } else
                     System.out.println("무승부");
             } else if (player01.RSC == 3) {
                 if (player02.RSC == 1) {
                     System.out.println("player02 win!");
-                    break;
+                    a=0;
                 } else if (player02.RSC == 2) {
                     System.out.println("player01 win!");
-                    break;
+                    a=0;
                 } else
                     System.out.println("무승부");
             }
